3472df0c6b961d4cec5241eee51aa3814e777765
Luciana B Ferrand <lbferrand@gmail.com>
2024-02-12 11:54:31 -0500

    Update  Index jsx to create accordian

---
+++ react-projects-25/src/components/accordian/data.js
@@ -0,0 +1,27 @@
+const data = [
+  {
+    id: "1",
+    question: "What are accordian components?",
+    answer:
+      "Accordian components are a type of UI component that allows you to show and hide content. They are useful for creating a collapsible section of content, such as a FAQ section or a list of items.",
+  },
+  {
+    id: "2",
+    question: "How do you use an accordian component?",
+    answer:
+      "To use an accordian component, you simply need to import the component and pass it the content you want to display. You can then customize the appearance and behavior of the accordion using props.",
+  },
+  {
+    id: "3",
+    question: "Accordian components vs tabs",
+    answer:
+      "Accordian components and tabs are similar in that they both allow you to show and hide content. However, accordian components are typically used for vertical layouts, while tabs are used for horizontal layouts.",
+  },
+  {
+    id: "4",
+    question: "Can you nest accordian components?",
+    answer:
+      "Yes, you can nest accordian components inside each other to create a multi-level accordion. This can be useful for organizing complex content into a hierarchical structure.",
+  },
+];
+export default data;
---
+++ react-projects-25/src/components/accordian/index.jsx
@@ -0,0 +1,32 @@
+//single select accordian
+//multiple select accordian
+
+import React, { useState } from "react";
+import data from "./data";
+
+export default function Accordian() {
+  const [selected, setSelected] = useState(null);
+
+  function handleSingleSelection(getCurrentId) {}
+  return (
+    <div className="wrapper">
+      <div className="accordian">
+        {data && data.length > 0 ? (
+          data.map((dataItem) => (
+            <div className="item">
+              <div
+                onClick={() => handleSingleSelection(dataItem.id)}
+                className="title"
+              >
+                <h3>{dataItem.question}</h3>
+                <span>+ </span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <div> No data found </div>
+        )}
+      </div>
+    </div>
+  );
+}
